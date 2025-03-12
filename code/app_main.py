46f523bfaeb8f69907b970f3798ee23e1a1c0180
alhakan sol <alhakansol@gmail.com>
2025-03-12 00:15:52 +0000

    added dependencies and created a couple of files
    added dependencies and created a couple of files. also,  i've created a basic app structure

---
+++ code/app_main.py
@@ -0,0 +1,21 @@
+from dash import Dash,html,dcc
+from dash.dependencies import Input,Output
+import dash_bootstrap_components  as dbc
+import pandas as pd
+import plotly as plt
+
+
+app=Dash(external_stylesheets=[dbc.themes.DARKLY])
+app.layout=html.Div(
+    children=[
+        html.Header('Dash App')
+        ]
+)
+
+
+
+
+
+
+if __name__=='__main__':
+    app.run(debug=True)
