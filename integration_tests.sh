f5101074d947ff13c5a751d1546c7793a344531d
alhakan sol <alhakansol@gmail.com>
2025-03-29 00:27:11 +0000

    testing

--- integration_tests.sh
+++ integration_tests.sh
@@ -10,11 +10,12 @@ check_git_changes() {
     if [[ -n $(git status --porcelain) ]]; then
         echo -e "\nCommitting changes..."
         sleep 1
+        git add .
         if git commit -m "$1"; then
                 echo -e "\nCommit successful!"
         else
             echo -e "\n❌ Commit operation failed"
-            exit 1
+            exit 0
         fi
     else
         echo "✅ Clean working directory"
@@ -26,34 +27,17 @@ check_git_changes() {
 
 
 git_push(){
-    check_git_changes
-    
+    check_git_changes $1
     sleep 1
-
-    echo "Testing GitHub SSH access..."
-    if ! ssh -T git@github.com 2>&1 | grep -q "successfully authenticated"; then
-        echo "❌ GitHub SSH authentication failed"
-        exit 1
-    else
-        echo "✅ GitHub SSH authentication successful"
-    fi
+    echo -e "\nPushing changes to GitHub..."
     sleep 1
-
-
-
-    if check_git_changes; then
-                echo -e "\nPushing changes to GitHub..."
-                sleep 1
-                if git push ; then
-                    echo -e "\n✅ Successfully pushed to GitHub"
-                    exit 0  
-                else
-                    echo -e "\n❌ Push failed"
-                    exit 1
-                fi  
+    if git remote set-url https://github.com/Alhakan501/quantium-starter-repo | git push ; then
+            echo -e "\n✅ Successfully pushed to GitHub"
+            exit 0  
     else
-        exit 0  
-    fi
+            echo -e "\n❌ Push failed"
+            exit 0
+    fi  
 
 
 }
